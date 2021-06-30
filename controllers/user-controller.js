9c73fcbd622fc58754b150259ebbc81f47e2519a
Illay Zelenkovsky <djzel9@yahoo.com>
2021-06-30 16:57:31 +0300

    express-validator>>valid email and password

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,8 +1,14 @@
 const userService = require('../service/user-service');
+const {validationResult} = require('express-validator');
+const ApiError = require('../exceptions/api-error');
 
 class UserController{
     async registratioin(req, res, next){
         try{
+            const errors = validationResult(req);
+            if(!errors.isEmpty()){
+                return next(ApiError.BadRequest('Ошибка валидации', errors.array()))
+            }
             const {email, password} = req.body;
             const userData = await userService.registration(email, password);
             res.cookie('refreshToken', userData.refreshToken, {maxAge: 30*24*60*60*1000, httpOnly: true})
@@ -56,7 +62,6 @@ class UserController{
     }
 }
 
-class UserControllerImpl extends UserController {
-}
+
 
 module.exports = new UserController();
