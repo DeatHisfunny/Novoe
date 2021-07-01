10b3bb0a29f44f10dbaa961860c954c9970ae696
Illay Zelenkovsky <djzel9@yahoo.com>
2021-07-01 13:36:35 +0300

    express-validator>>middleware auth fix

---
+++ middlewares/auth-middleware.js
@@ -0,0 +1,25 @@
+const ApiError = require('../exceptions/api-error');
+const tokenService = require('../service/token-service');
+
+module.exports = function (req, res, next){
+    try{
+        const authorizationHeaer = req.headers.authorization;
+        if(!authorizationHeaer){
+            return next(ApiError.UnauthorizedError());
+        }
+
+        const accessToken = authorizationHeaer.split(' ')[1];
+        if(!accessToken){
+            return next(ApiError.UnauthorizedError());
+        }
+        const userData = tokenService.validateAccessToken(accessToken);
+        if(!userData){
+            return next(ApiError.UnauthorizedError());
+        }
+        req.user = userData;
+         next();
+    }catch(e){
+        return next(ApiError.UnauthorizedError());
+    }
+
+}
